487305b9e007b6bcd806b4051e7ce1096db74b85
classehazel <classehazel@gmail.com>
2023-04-19 13:20:55 -0500

    My second react website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Membership from './pages/Membership';
-import Program from './pages/Program';
-import Gallery from './pages/Gallery'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Membership from "./pages/Membership";
+import Program from "./pages/Program";
+import Gallery from "./pages/Gallery";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/membership" element={<Membership />} />
-        <Route path="/program" element={<Program />} />
-        <Route path="/gallery" element={<Gallery />} />
-      </Routes>
-      <Footer />
-    </Router>
-        </>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/membership" element={<Membership />} />
+          <Route path="/program" element={<Program />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
